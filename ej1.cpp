e21d6f515841b49c1dd3d1405e580b71092b4256
benitaeldegui <alumno26.serrano.benjamin@ipm.edu.ar>
2024-04-16 10:17:44 -0300

    ejercicio 1

---
+++ ej1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std; 
+
+int main(){
+    float numero_1, numero_2 ,promedio;
+    cout<< "ingrese numero1:"<<endl;
+    cin>> numero_1;
+    cout<< "ingrese numero2:"<<endl;
+    cin>> numero_2;
+    promedio = (numero_1 + numero_2)/2;
+    cout<< "promedio:";
+    cout<< promedio<<endl;
+}
+
