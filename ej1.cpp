a41cc6bc0e3fc56efa1d4482cc5e3dc629c22d53
benitaeldegui <alumno26.serrano.benjamin@ipm.edu.ar>
2024-04-16 10:19:31 -0300

    cambio ej 1

--- ej1.cpp
+++ ej1.cpp
@@ -5,9 +5,8 @@ int main(){
     float numero_1, numero_2 ,promedio;
     cout<< "ingrese numero1:"<<endl;
     cin>> numero_1;
-    cout<< "ingrese numero2:"<<endl;
-    cin>> numero_2;
-    promedio = (numero_1 + numero_2)/2;
+
+    promedio = (numero_1 + 5)/2;
     cout<< "promedio:";
     cout<< promedio<<endl;
 }
